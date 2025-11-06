cdd73ed1f2ba076207e3b16ec9d3ec33c22af59e
sowonlee0812-coder <sowon.lee0812@gmail.com>
2025-11-06 15:43:41 +0900

    project10-5

--- project10-1/project10-1/main.c
+++ project10-1/project10-1/main.c
@@ -4,11 +4,26 @@
 #include <string.h>
 
 int main(int argc, char * argv[]) {
-    char str[30] = "happy C programming";
     
-    printf("string %s length : %i\n",
-           str, strlen(str));
+    char input[100];
+    FILE* fp;
+    int i;
     
+     //fopen
+    fp = fopen("sample.txt", "w");
+    
+    for (i=0;i<3;i++)
+    {
+        //fprintf
+        printf("input a word : ");
+        scanf("%s", input);    //배열은 &안넣어도됨 input 자체가 포인터임
+        fprintf(fp, "%s\n", input);
+        
+    }
+    
+        //fclose
+        fclose(fp);
+
     system("PAUSE");
     return 0;
     }
